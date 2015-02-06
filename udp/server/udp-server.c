f26de8d4f0284524418c0820f2acebab5e2f79c9
Nick Keller <nick.keller57@gmail.com>
2015-02-05 21:01:47 -0500

    Server now binds on specified port, with ip addr 127.0.0.1

--- udp/server/udp-server.c
+++ udp/server/udp-server.c
@@ -7,12 +7,49 @@ int main(int argc, char *argv[]){
 		print_use_and_exit();
 	}
 	
-	int port = argv[1];
-	printf("Binding server on port %d\n",port);
+	char* port = argv[1];
+	//create a socket to bind on
+	CONN_INFO* connection = setup_socket(port);
+	printf("Binding server on port %s\n",port);
+	int bindResult = bind(connection->socket,connection->remote_addr,connection->addrlen);
+	if(bindResult == 0){
+		printf("Ready to receive on port %s\n",port);
+	}
+	else{
+		printf("Binding failed\n");
+	}
 	return 0;
 }
 
 void print_use_and_exit(){
 	fprintf(stderr,"Usage:  server-udp port\n\n");
 	exit (EXIT_FAILURE);
+}
+
+CONN_INFO* setup_socket(char* port){
+	struct addrinfo *connections, *conn = NULL;
+	struct addrinfo info;
+	memset(&info, 0, sizeof(struct addrinfo));
+	int sock = 0;
+	
+	info.ai_family = AF_INET;
+	info.ai_socktype = SOCK_DGRAM;
+	info.ai_protocol = IPPROTO_UDP;
+	getaddrinfo("127.0.0.1",port,&info,&connections);
+	for(conn = connections; conn != NULL; conn = conn->ai_next){
+		sock =  socket(conn->ai_family, conn->ai_socktype, conn->ai_protocol);
+		if(sock < 0){
+			printf("Failed to create socket\n");
+			continue;
+		}
+		break;
+	}
+	if(conn == NULL){
+		printf("Failed to create socket\n");
+	}
+	CONN_INFO* conn_info = malloc(sizeof(CONN_INFO));
+	conn_info->socket = sock;
+	conn_info->remote_addr = conn->ai_addr;
+	conn_info->addrlen = conn->ai_addrlen;
+	return conn_info;	
 }
--- udp/server/udp-server.h
+++ udp/server/udp-server.h
@@ -3,14 +3,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <string.h>
 
 #ifndef UDP_SERVER_H
 #define UDP_SERVER_H
 
 enum {REQ=0, CHA, RESP, ACK};
 
+typedef struct _CONN_INFO{
+	int socket;
+	socklen_t addrlen;
+	struct sockaddr *remote_addr;
+} CONN_INFO;
 
+//function declarations
 void print_use_and_exit(void);
+CONN_INFO* setup_socket(char* port);
 
 
 #endif//end udp-server.h
